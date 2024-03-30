60170a52036c1b98b45e466cf3968a7f19ab09c6
Liu <zehao100@qq.com>
2024-03-31 03:07:21 +0800

    ✨ Refactor Render component to use Markmap instead of markmap-lib and markmap-view in Render.tsx

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -1,7 +1,6 @@
 import { memo, useEffect, useRef } from 'react';
 import { Flexbox } from 'react-layout-kit';
-import { transform, createViewer } from 'markmap-lib';
-import { loadPlugins } from 'markmap-view';
+import { Markmap } from 'markmap-view';
 
 import { ResponseData } from '@/type';
 
@@ -10,14 +9,8 @@ const Render = memo<Partial<ResponseData>>(({ content }) => {
 
   useEffect(() => {
     if (containerRef.current) {
-      // Transform the markdown content into markmap data
-      const { root, features } = transform(content || '');
-
-      // Load necessary plugins based on the features used in the content
-      const { Viewer } = loadPlugins(features);
-
       // Create a new markmap in the container
-      const viewer = new Viewer(containerRef.current, null, root);
+      const viewer = Markmap.create(containerRef.current, null, content || '');
 
       // You might want to save the viewer instance if you need to interact with it later
       // For example, you can update the markmap content by calling viewer.setData()
