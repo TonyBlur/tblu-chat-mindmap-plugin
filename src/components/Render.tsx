69590f5ef960e49028edc8f0338f46f4254d82e6
Liu <zehao100@qq.com>
2024-03-31 01:35:38 +0800

    ✨ Refactor Render component to update iframe style and remove unused code in Render.tsx

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -1,49 +1,25 @@
 import LZString from 'lz-string';
-import { memo, useEffect, useState } from 'react';
-import ReactSVG from 'react-svg';
+import { memo } from 'react';
 // import { Flexbox } from 'react-layout-kit';
-import axios from 'axios';
-import { JSDOM } from 'jsdom';
 
 import { ResponseData } from '@/type';
 
 const Render = memo<Partial<ResponseData>>(({ content }) => {
-  const [svgContent, setSvgContent] = useState('');
   const url = `https://markmap-renderer.vercel.app/?content=${LZString.compressToEncodedURIComponent(content || '')}`;
 
-  useEffect(() => {
-    axios.get(url)
-      .then(response => {
-        const dom = new JSDOM(response.data);
-        const svgElement = dom.window.document.querySelector('svg');
-        if (svgElement) {
-          setSvgContent(svgElement.outerHTML);
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching HTML:', error);
-      });
-  }, [url]);
-
   return (
-    <ReactSVG src={`data:image/svg+xml,${encodeURIComponent(svgContent)}`} />
+    <iframe 
+      frameBorder="0"
+      src={url}
+      style={{ 
+        borderRadius: '8px',
+        height: '100vh', 
+        width: '100%'
+      }}
+      >
+        您的浏览器不支持iframe
+    </iframe>
   );
-
-  // return (
-  //   <Flexbox width="100%">
-  //     <iframe 
-  //       frameBorder="0"
-  //       src={url}
-  //       style={{ 
-  //         borderRadius: '8px',
-  //         height: '100vh', 
-  //         width: '100%'
-  //       }}
-  //       >
-  //         您的浏览器不支持iframe
-  //     </iframe>
-  //   </Flexbox>
-  // );
 });
 
 export default Render;
