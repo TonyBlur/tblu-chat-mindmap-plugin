44ad42901c5b3a844507a7070854a0232024cc90
Liu <zehao100@qq.com>
2024-03-30 19:18:48 +0800

    ✨ Refactor Render component: remove iframe and redirect iframe to markmap-render url

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -1,40 +1,16 @@
 import LZString from 'lz-string';
-import { memo } from 'react';
-import { Flexbox } from 'react-layout-kit';
+import { memo, useEffect } from 'react';
 
 import { ResponseData } from '@/type';
 
 const Render = memo<Partial<ResponseData>>(({ content }) => {
   const url = `https://markmap-renderer.vercel.app/?content=${LZString.compressToEncodedURIComponent(content || '')}`;
-  return (
-    <Flexbox width="100%">
-      {/* <Flexbox distribution={'space-between'} horizontal>
-        🌟心情：{mood}
-        <span className={styles.date}>{dayjs(today).format('YYYY/MM/DD')}</span>
-      </Flexbox>
-      <Flexbox gap={8}>
-        推荐衣物abc
-        <Flexbox gap={12} horizontal style={{ overflow: 'scroll' }}>
-          {clothes?.map((item) => (
-            <Card key={item.name} size={'small'} title={item.name}>
-              {item.description}
-            </Card>
-          ))}
-        </Flexbox>
-      </Flexbox> */}
-      <iframe 
-        frameBorder="0"
-        src={url}
-        style={{ 
-          borderRadius: '8px',
-          height: '100vh', 
-          width: '100%'
-        }}
-        >
-          您的浏览器不支持iframe
-      </iframe>
-    </Flexbox>
-  );
+
+  useEffect(() => {
+    window.parent.location.href = url;
+  }, [url]);
+
+  return null;
 });
 
 export default Render;
